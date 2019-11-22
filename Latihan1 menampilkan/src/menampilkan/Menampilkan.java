3c081b2f97076e222eb448af9afe47ddc4dfde57
DeniRomansyah71 <dennyzromansyah71@gmail.com>
2019-11-23 04:29:23 +0700

    File Final

---
+++ Latihan1 menampilkan/src/menampilkan/Menampilkan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menampilkan;
+
+/**
+ Nama : DENI ROMANSYAH
+ KELAS : TI-1C
+ NIM : A2.1900043
+ */
+public class Menampilkan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Biodata Rockstar" + "\n" + "-------------------");
+        System.out.println("NAMA\t\t: Deni Romansyah");
+        System.out.println("ALAMAT\t\t: Rancakalong");
+        System.out.println("TEMPAT LAHIR\t: Sumedang ");
+        System.out.println("TANGGAL LAHIR\t: 15 april 2000");
+        System.out.println("-------------------");
+        System.out.println("HOBI\t\t: Bermain alat musik");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+}
